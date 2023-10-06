457a2bd5af680d697147cb270b23fbbb6cfcc420
peterdemespacelift <108002504+peterdemespacelift@users.noreply.github.com>
2023-10-06 12:24:33 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "eu-central-1"
-}
-
-resource "aws_ssm_parameter" "testparam" {
-    name = "peterdeme-test-parameter"
-    type = "String"
-    value = "foobar123"
-
-    tags = {
-        developer = "peterdeme"
-        env_name = "development"
-    }
-}
-
 resource "random_password" "password" {
   count = 2
   length = 5
